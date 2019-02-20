cb555b9fbb8d8f9059d65f0577567da5faf44985
Tony_Huang <tong@Tony-Huangs-MacBook-Pro.local>
2019-02-19 19:50:51 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Your code goes here
+ const p = document.getElementById('text');
+
+  const domContentLoaded = () =>{
+    document.addEventListener("DOMContentLoaded", event => {
+        updateDOM()
+    });
+  }
+
+  function updateDOM(){
+      p.innerHTML="This is really cool!"
+  }
